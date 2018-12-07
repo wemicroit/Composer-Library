a16acff282c13df71ac05442b538a671ebfe8395
James Thompson <thompson.tomo@outlook.com>
2018-12-07 11:10:18 +1100

    Build out of required functionality (#1)
    Commits:
    
    * Started working on building the library just need to finalise parsing
    
    * Tweaks to get it working.
    
    * Major refactor
    
    * Tweaks to models
    
    * set a default
    
    * Spelling fixes.
    
    * Implemented the new settings class to contain information regarding all iterations to do.
    
    * Extensive reworking of process logic to take into account the new ability to perform multiple actions. This is vital for our ability to generate the documents.
    
    * More methods for the cycle
    
    * Added more cycle types to make it work better
    
    * Introduced ability to convert input file on load. Not implement
    
    * Renamed properties on cycle
    
    * Rename spaced enums
    
    * Renamed assembly so no to include developer
    
    * Made a bunch of fields rather than methods
    
    * Made new constructor which accepts action type
    
    * Added in a preserve file property for renaming
    
    * Model tidy up
    
    * First Pass at documenting everything.
    
    - Tweaks for new library
    - Added empty files
    - Documented enums
    - Document both the constructors and the properties.
    - Document the methods of the class
    - Document the ComposeData
    - Reorganised cycle into preferred order.
    - Document model and constructor for Cycle
    - Document the properties of cycle
    - Spelling fixes
    - New Compiled Version.
    
    * fixed packages
    
    * Added in License
    
    * Restructured to be more logical
    
    * Added in Logo for package

---
+++ Composer/Compose.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeMicroIt.Utils.Composer.Models;
+using WeMicroIt.Utils.Composer.Resources.Enums;
+using WeMicroIt.Utils.FileConverter;
+
+namespace WeMicroIt.Utils.Composer
+{
+    /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/class/*'/> /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/class/*'/> 
+    public class Compose
+    {
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/properties[@name="Templater"]/*'/> 
+        public string Templater
+        {
+            set
+            {
+                fileComms.Templater = value;
+            }
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/properties[@name="Reader"]/*'/> 
+        public string Reader
+        {
+            set
+            {
+                fileComms.Reader = value;
+            }
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/properties[@name="Writer"]/*'/> 
+        public string Writer
+        {
+            set
+            {
+                fileComms.Writer = value;
+            }
+        }
+
+        private FileManager fileComms { get; set; }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/constructor[@name="Default"]/*'/> 
+        public Compose()
+        {
+            fileComms = new FileManager();
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/methods[@name="Process"]/*'/> 
+        public bool Process(ComposeData Data)
+        {
+            foreach (var cycle in Data.Cycles)
+            {
+                switch (cycle.InputType)
+                {
+                    case GenerationType.JSON:
+                        break;
+                    case GenerationType.CSV:
+                        break;
+                    case GenerationType.Raw:
+                        continue;
+                }
+                switch (cycle.ActionType)
+                {
+                    case ActionType.Unknown:
+                        continue;
+                    case ActionType.Transform:
+                        SetStreams(cycle.SourcePath, cycle.FullPath, cycle.TemplatePath);
+                        fileComms.TransformXML();
+                        break;
+                    case ActionType.Split:
+                        SetStreams(cycle.SourcePath, cycle.FullPath);
+                        fileComms.SplitXML(cycle.QueryPath, cycle.NewFileName);
+                        break;
+                    case ActionType.SplitAndTransform:
+                        SetStreams(cycle.SourcePath, cycle.FullPath, cycle.TemplatePath);
+                        fileComms.SplitAndTransformXML(cycle.QueryPath, cycle.GroupPath, cycle.NewFileName);
+                        break;
+                    case ActionType.FileRename:
+                        SetStreams(cycle.SourcePath, cycle.DestinationDir);
+                        fileComms.ConvertXMLFolderToRaw(cycle.Preserve);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return true;
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/methods[@name="SetStreams"][@version="Both"]/*'/> 
+        public bool SetStreams(string read, string write)
+        {
+            Reader = read;
+            Writer = write;
+            return true;
+        }
+
+        /// <include file='./docs/Classes.xml' path='doc/classes[@name="Compose"]/methods[@name="SetStreams"][@version="All"]/*'/> 
+        public bool SetStreams(string read, string write, string template)
+        {
+            Reader = read;
+            Writer = write;
+            Templater = template;
+            return true;
+        }
+    }
+}
---
+++ Composer/Resources/Enums/GenerationType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeMicroIt.Utils.Composer.Resources.Enums
+{
+    /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/enum/*'/> 
+    public enum GenerationType
+    {
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="Markdown"]/*'/> 
+        Markdown = 0,
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="XML"]/*'/> 
+        XML = 1,
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="HTML"]/*'/> 
+        Html = 2,
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="JSON"]/*'/> 
+        JSON = 3,
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="CSV"]/*'/> 
+        CSV = 4,
+        /// <include file='./docs/Enums.xml' path='doc/enums[@name="GenerationType"]/field[@name="Raw"]/*'/> 
+        Raw = 5
+    }
+}
